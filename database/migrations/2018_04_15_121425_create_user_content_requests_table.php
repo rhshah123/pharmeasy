56c1f344dc6dff7c9b333cd4998731015e14eba6
Rohan Shah <rhshah123@gmail.com>
2018-04-16 16:20:12 +0530

    Update 2018_04_15_121425_create_user_content_requests_table.php

--- database/migrations/2018_04_15_121425_create_user_content_requests_table.php
+++ database/migrations/2018_04_15_121425_create_user_content_requests_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserContentResuestsTable extends Migration
+class CreateUserContentRequestsTable extends Migration
 {
     /**
      * Run the migrations.
